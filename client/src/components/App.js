2f87a2e748bbee3a8439f89a9a250b320b8e8a99
Sunrise <sunrise4ws@gmail.com>
2020-09-24 03:53:09 -0500

    login and sidebar started

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Login from "./Login";
+import useLocalStorage from "../hooks/useLocalStorage";
+import Dashboard from "./Dashboard";
 
 function App() {
-  return <Login />;
+  const [id, setId] = useLocalStorage("id");
+
+  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
 }
 
 export default App;
---
+++ client/src/components/NewConversationModal.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NewConversationsModal() {
+  return <div>test</div>;
+}
