c6ba5310e942f8d88d5eb301718f13c7e7f2677a
Sunrise <sunrise4ws@gmail.com>
2020-09-25 22:04:30 -0500

    small tweaks

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function OpenConversation() {
-  return <div></div>;
+  return (
+    <div className="d-flex flex-column flex-grow-1">
+      <div className="flex-grow-1 overflow-auto"></div>
+    </div>
+  );
 }
